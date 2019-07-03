68c3565125cbe373b971364ed1b696e86e74ee78
Orlof <orlof@mbnet.fi>
2019-07-03 10:52:38 +0300

    changed docker image name

--- example_docker_run_cat/docker_image/build.sh
+++ example_docker_run_cat/docker_image/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t robocorp/robocloud-runtime .
+docker build -t robocorp/robocloud-generic-task-executor .
--- example_docker_run_cat/scripts/robocloud_runner.sh
+++ example_docker_run_cat/scripts/robocloud_runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -v $(pwd)/bundle:/bundle:rw robocorp/robocloud-runtime scripts/prepare_and_run.sh
+docker run -v $(pwd)/bundle:/bundle:rw robocorp/robocloud-generic-task-executor scripts/prepare_and_run.sh
 mv bundle/output ../output
