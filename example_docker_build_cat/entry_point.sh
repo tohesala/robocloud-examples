366912f3b2a5a97f188d10dfe327a058f0804f6c
Orlof <orlof@mbnet.fi>
2019-07-03 09:31:06 +0300

    change from xvbf to headless chrome

--- example_docker_build_cat/entry_point.sh
+++ example_docker_build_cat/entry_point.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-GEOMETRY="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}"
-Xvfb :99 -screen 0 $GEOMETRY &
-export DISPLAY=:99
+#GEOMETRY="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}"
+#Xvfb :99 -screen 0 $GEOMETRY &
+#export DISPLAY=:99
 
 cd /bundle
 scripts/prepare_and_run.sh
