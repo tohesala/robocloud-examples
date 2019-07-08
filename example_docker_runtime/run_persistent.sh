a59f2f2d3ec32e53a9cbf5fd467d9514d7f7ddba
Orlof <orlof@mbnet.fi>
2019-07-08 22:17:56 +0300

    interface for multiple containers

--- example_docker_runtime/run_persistent.sh
+++ example_docker_runtime/run_persistent.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-if [[ ! -d "instance" ]]; then
-    mkdir instance
-    cp rtconfig.yml instance/
-    docker run -v $(pwd)/instance:/instance:rw robocorp/robocloud-runtime link $@
+if [ $# -eq 2 ]
+  then
+    rm -rf $1
+    mkdir $1
+    cp rtconfig.yml $1/
+    nohup docker run -v $(pwd)/$1:/instance:rw robocorp/robocloud-runtime link $2 &
+    echo Started runtime instance $1
+elif [ $# -eq 1 ]
+  then
+    if [[ ! -d "$1" ]]
+      then
+        echo $1 is not initialized with link token
+        exit
+    else
+      nohup docker run -v $(pwd)/$1:/instance:rw robocorp/robocloud-runtime &
+      echo Started runtime instance $1
+    fi
 else
-    docker run -v $(pwd)/instance:/instance:rw robocorp/robocloud-runtime
+  echo Usage: ./run_persistent.sh instance_path [link-token]
 fi
